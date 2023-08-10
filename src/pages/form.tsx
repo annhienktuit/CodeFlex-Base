892f764fdc7e30952c13a9b8f7b68ec95de5b4c1
nhiennha <nhiennha@vng.com.vn>
2023-08-11 01:16:00 +0700

    add firebase

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -3,7 +3,8 @@ import { Button, Input, Box, Page, useSnackbar } from "zmp-ui";
 import { useRecoilState } from "recoil";
 import { userInfo } from "zmp-sdk";
 import { userState } from "../state";
-
+import fbApp from "../firebase";
+import { getDatabase, ref, set } from "firebase/database";
 type UserForm = Omit<userInfo, "id">;
 
 const FormPage: React.FunctionComponent = () => {
@@ -23,6 +24,15 @@ const FormPage: React.FunctionComponent = () => {
   const handleSubmit = () => {
     snackbar.openSnackbar({ duration: 3000, text: "saved", type: "success" });
     setUser((user) => ({ ...user, ...form }));
+  };
+
+  const pushSampleData = async () => {
+    const db = getDatabase(fbApp);
+    set(ref(db, "users/" + "nhien"), {
+      username: "annhienkt",
+      email: "annhienkt@gmail.com",
+      profile_picture: "123",
+    });
   };
 
   return (
@@ -45,7 +55,14 @@ const FormPage: React.FunctionComponent = () => {
             onChange={(e) => handleChangeInput("avatar", e.target.value)}
           />
           <Box mt={4}>
-            <Button fullWidth variant="primary" onClick={handleSubmit}>
+            <Button
+              fullWidth
+              variant="primary"
+              onClick={() => {
+                pushSampleData();
+                handleSubmit;
+              }}
+            >
               Submit
             </Button>
           </Box>
