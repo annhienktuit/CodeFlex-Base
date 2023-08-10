d085a956f19eaec298e50f26c57572200ae0e42f
thienttt <thientf.97@gmail.com>
2023-08-11 01:31:34 +0700

    fix predefined colorpalette

--- src/utils/appStyles.ts
+++ src/utils/appStyles.ts
@@ -30,15 +30,15 @@ export type ColorDefinition = {
 };
 
 export const colorPalette: Record<string, ColorDefinition> = {
-  primary: { color: '#6DBE45', className: 'text-primary' },
-  secondary: { color: '#F4A623', className: 'text-secondary' },
-  background: { color: '#FFFFFF', className: 'bg-background' },
-  text: { color: '#333333', className: 'text-text' },
-  accent1: { color: '#E71D36', className: 'text-accent1' },
-  accent2: { color: '#FF9F1C', className: 'text-accent2' },
-  accent3: { color: '#2EC4B6', className: 'text-accent3' },
-  neutral1: { color: '#C0C0C0', className: 'text-neutral1' },
-  neutral2: { color: '#E5E5E5', className: 'text-neutral2' },
+  primary: { color: '#6DBE45', className: 'primary' },
+  secondary: { color: '#F4A623', className: 'secondary' },
+  background: { color: '#FFFFFF', className: 'background' },
+  text: { color: '#333333', className: 'text' },
+  accent1: { color: '#E71D36', className: 'accent1' },
+  accent2: { color: '#FF9F1C', className: 'accent2' },
+  accent3: { color: '#2EC4B6', className: 'accent3' },
+  neutral1: { color: '#C0C0C0', className: 'neutral1' },
+  neutral2: { color: '#E5E5E5', className: 'neutral2' },
 };
 
 export default colors;
